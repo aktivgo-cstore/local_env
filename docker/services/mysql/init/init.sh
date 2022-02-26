9b4381a35f23c4d327703416b846100d6a213d6c
aktivgo <aktivgo01062001@gmail.com>
2022-02-26 23:24:25 +0300

    Fix

--- docker/services/mysql/init/init.sh
+++ docker/services/mysql/init/init.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#mysql --user="dev" --password="dev" --host="cstore_mysql" --execute="CREATE DATABASE IF NOT EXISTS cstore; \
-#    GRANT ALL PRIVILEGES ON cstore.* TO 'dev'@'localhost';"
 mysql -uroot -proot --host="cstore_mysql" --execute="CREATE DATABASE IF NOT EXISTS cstore; \
     GRANT ALL PRIVILEGES ON cstore.* TO root@localhost;"
